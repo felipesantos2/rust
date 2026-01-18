7effd659140bc030fb169ffb46aa484905147e88
felipesantos2 <santospinheiro627@gmail.com>
2026-01-18 15:55:36 -0300

    testes com tipos primitivos

--- src/dump/primitive.rs
+++ src/dump/primitive.rs
@@ -1,16 +1,23 @@
 // tipos escalares X tipos compostos
-// 
-// inteiro
-// flutuante
+//
+// inteiro, signed -> i e unsigned -> u
+// flutuante, f32 e f34
 // booleano
 // caractere
-// 
-// tupas
-// arrays
-// 
+// let caractere = 'f'
 
+// fn my_func() -> char {
+//     let charr: char = 'f'
+// }
 
+//
+// tupas -> agrapa tipos diferentes, mas com tamanho fixo
 
+// fn my_funct2() {
+//     (true, true, false);
+// }
 
-
-
+// arrays -> agrupa o mesmo tipo, também com tamanho fixo
+//
+// const a: [i32; 5] = [1,2,4,6,9];
+//
